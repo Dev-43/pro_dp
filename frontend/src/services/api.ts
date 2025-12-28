704346750de147173dba3dfb9784cef9edf24130
Dev-43 <deveshdolas9@gmail.com>
2025-12-28 15:49:06 +0530

    API

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://pro-dp-1.onrender.com/";
+const API_BASE_URL = "https://pro-dp-1.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
